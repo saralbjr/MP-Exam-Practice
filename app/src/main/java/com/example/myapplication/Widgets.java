be955c6a33a27fbae19986b3cfef217a7b47b46d
Saral Bajracharya <rmanbajra@gmail.com>
2024-09-01 15:43:39 +0545

    Exam practice

--- app/src/main/java/com/example/myapplication/Widgets.java
+++ app/src/main/java/com/example/myapplication/Widgets.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class Widgets extends AppCompatActivity {
 
@@ -16,5 +21,29 @@ public class Widgets extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_widgets);
 
+
+        Spinner spinner=findViewById(R.id.spinner);
+        String items[]={"Item 1","Item 2","Item 3","Item 4"};
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>
+                (this,R.layout.activity_widgets,items);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Get the selected item
+                String selectedItem = parentView.getItemAtPosition(position).toString();
+                // Display a Toast with the selected item
+                Toast.makeText(Widgets.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+
+        });
     }
 }
