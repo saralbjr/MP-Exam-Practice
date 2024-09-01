48d5ee31395e1fff7453f985e1b86523e07c931a
Saral Bajracharya <rmanbajra@gmail.com>
2024-09-01 12:14:38 +0545

    Exam practice

--- app/src/main/java/com/example/myapplication/TableLayout.java
+++ app/src/main/java/com/example/myapplication/TableLayout.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RelativeLayout extends AppCompatActivity {
+public class TableLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_relative_layout);
+        setContentView(R.layout.activity_table_layout);
 
     }
 }
